5c983f7fec5175abfbb0a14fe0b56bb036cefca7
waltsu <waltsumarcus@gmail.com>
2026-06-24 19:33:26 +0300

    refactor: fix texts

--- course-information/src/App.jsx
+++ course-information/src/App.jsx
@@ -48,6 +48,7 @@ const App = () => {
 
   return (
     <div>
+      <h1>Web development curriculum</h1>
       {courses.map((course) => (
         <Course key={course.id} course={course} />
       ))}
--- course-information/src/Course.jsx
+++ course-information/src/Course.jsx
@@ -1,31 +1,30 @@
+const Header = ({ parts }) => <h3>{parts.name}</h3>
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  const courseList = parts.map((part) => {
+    return <Part key={part.id} name={part.name} exercises={part.exercises} />
+  })
+
+  return <div>{courseList}</div>
+}
+
+const Total = ({ parts }) => {
+  const initialValue = 0
+  const exercisesTotal = parts.reduce(
+    (sum, part) => sum + part.exercises,
+    initialValue
+  )
+  return <b>total of exercises {exercisesTotal}</b>
+}
 const Course = ({ course }) => {
-  const Header = ({ parts }) => <h1>{parts.name}</h1>
-
-  const Part = ({ name, exercises }) => {
-    return (
-      <p>
-        {name} {exercises}
-      </p>
-    )
-  }
-
-  const Content = ({ parts }) => {
-    const courseList = parts.map((part) => {
-      return <Part key={part.id} name={part.name} exercises={part.exercises} />
-    })
-
-    return <div>{courseList}</div>
-  }
-
-  const Total = ({ parts }) => {
-    const initialValue = 0
-    const exercisesTotal = parts.reduce(
-      (sum, part) => sum + part.exercises,
-      initialValue
-    )
-    return <b>total of exercises {exercisesTotal}</b>
-  }
-
   return (
     <div>
       <Header parts={course} />
